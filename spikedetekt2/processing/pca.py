e3287235ae9047d938949183204c7737c43fa225
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-07 14:18:06 +0200

    Added PCA projection routine.

--- spikedetekt2/processing/pca.py
+++ spikedetekt2/processing/pca.py
@@ -20,11 +20,27 @@ def compute_pcs(x, npcs=None):
         return np.dstack([compute_pcs(x[..., i]) for i in range(x.shape[-1])])
     # Now, we assume x is a 2D array.
     assert x.ndim == 2
+    # Take the covariance matrix.
     cov_ss = np.cov(x.astype(np.float64), rowvar=0)
+    # Compute the eigenelements.
     vals, vecs = np.linalg.eigh(cov_ss)
     pcs = vecs.T.astype(np.float32)[np.argsort(vals)[::-1]]
+    # Take the first npcs components.
     if npcs is not None:
         return pcs[:npcs]
     else:
         return pcs
+    
+def project_pcs(x, pcs, npcs=None):
+    """Project data points onto principal components.
+    
+    Arguments:
+      * x: a 2D array.
+      * pcs: the PCs as returned by `compute_pcs`.
+    
+    """
+    x_proj = np.einsum('ijk,jk->ki', pcs, x)  # Notice the transposition.
+    x_proj *= 100.
+    return x_proj
+
     
--- spikedetekt2/processing/tests/test_pca.py
+++ spikedetekt2/processing/tests/test_pca.py
@@ -5,7 +5,7 @@
 # -----------------------------------------------------------------------------
 import numpy as np
 from scipy import signal
-from spikedetekt2.processing import compute_pcs
+from spikedetekt2.processing import compute_pcs, project_pcs
 
 
 # -----------------------------------------------------------------------------
@@ -21,6 +21,7 @@ def test_compute_pcs():
     x = np.dot(x, rot)
     # Compute the PCs.
     pcs = compute_pcs(x)
+    assert pcs.ndim == 2
     assert (np.abs(pcs) - a).max() < 1e-3
     
 def test_compute_pcs_3d():
@@ -30,9 +31,20 @@ def test_compute_pcs_3d():
     x = np.dstack((x1, x2))
     # Compute the PCs.
     pcs = compute_pcs(x)
+    assert pcs.ndim == 3
     assert np.linalg.norm(pcs[0,:,0] - np.array([-1., 0.])) < 1e-2
     assert np.linalg.norm(pcs[1,:,0] - np.array([0., -1.])) < 1e-2
     assert np.linalg.norm(pcs[0,:,1] - np.array([0, 1.])) < 1e-2
     assert np.linalg.norm(pcs[1,:,1] - np.array([-1., 0.])) < 1e-2
+    
+def test_project_pcs():
+    x1 = np.random.randn(20000, 2) * np.array([[10., 1.]])
+    x2 = np.random.randn(20000, 2) * np.array([[1., 10.]])
+    x = np.dstack((x1, x2))
+    # Compute the PCs.
+    pcs = compute_pcs(x)
+    # Project the PCs.
+    x_proj = project_pcs(x[0,...], pcs)
+    assert x_proj.shape == (2, 2)
     
     
