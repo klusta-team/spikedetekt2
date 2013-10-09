e4ef35b2ec8c9bfcf52704cf6bc610a373ba29ef
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-09 14:53:01 +0200

    Updated the file format diagram. Added some chunking functions and tests.

--- spikedetekt2/dataio/chunks.py
+++ spikedetekt2/dataio/chunks.py
@@ -9,5 +9,32 @@ import numpy as np
 # -----------------------------------------------------------------------------
 # Chunking functions
 # -----------------------------------------------------------------------------
+#m chops n_samples into chunks according to chunk_size,overlap
+#m Overlap probably controls for the artifacts of filtering on the ends of the signal?
+def chunk_bounds(n_samples, chunk_size, overlap=0):
+    '''
+    Returns chunks of the form:
+    [ overlap/2 | chunk_size-overlap | overlap/2 ]
+    s_start   keep_start           keep_end     s_end
+    Except for the first and last chunks which do not have a left/right overlap
+    '''
+    s_start = 0
+    s_end = chunk_size
+    keep_start = s_start
+    keep_end = s_end-overlap//2
+    yield s_start,s_end,keep_start,keep_end
+    
+    while s_end-overlap+chunk_size < n_samples:
+        s_start = s_end-overlap
+        s_end = s_start+chunk_size
+        keep_start = keep_end
+        keep_end = s_end-overlap//2
+        yield s_start,s_end,keep_start,keep_end
+        
+    s_start = s_end-overlap
+    s_end = n_samples
+    keep_start = keep_end
+    keep_end = s_end
+    yield s_start,s_end,keep_start,keep_end
 
 
--- spikedetekt2/dataio/tests/test_chunks.py
+++ spikedetekt2/dataio/tests/test_chunks.py
@@ -4,12 +4,18 @@
 # Imports
 # -----------------------------------------------------------------------------
 import numpy as np
-from spikedetekt2.dataio import *
+from spikedetekt2.dataio import chunk_bounds
 
 
 # -----------------------------------------------------------------------------
 # Chunking Tests
 # -----------------------------------------------------------------------------
-def test_chunk_1():
-    pass
-
+def test_chunk_bounds():
+    # for i, (s0, s1, keep0, keep1) in enumerate(
+            # chunk_bounds(1010, 100, overlap=20)):
+        
+    chunks = chunk_bounds(1010, 100, overlap=20)
+    
+    assert next(chunks) == (0, 100, 0, 90)
+    assert next(chunks) == (80, 180, 90, 170)
+    
