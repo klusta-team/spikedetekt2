2d2208af4b6b3dc8b923beda858c5b6cf51927a1
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-09 15:41:09 +0200

    Updated test in test_chunks.

--- spikedetekt2/dataio/tests/test_chunks.py
+++ spikedetekt2/dataio/tests/test_chunks.py
@@ -11,11 +11,9 @@ from spikedetekt2.dataio import chunk_bounds
 # Chunking Tests
 # -----------------------------------------------------------------------------
 def test_chunk_bounds():
-    # for i, (s0, s1, keep0, keep1) in enumerate(
-            # chunk_bounds(1010, 100, overlap=20)):
-        
-    chunks = chunk_bounds(1010, 100, overlap=20)
+    chunks = chunk_bounds(200, 100, overlap=20)
     
     assert next(chunks) == (0, 100, 0, 90)
     assert next(chunks) == (80, 180, 90, 170)
+    assert next(chunks) == (160, 200, 170, 200)
     
