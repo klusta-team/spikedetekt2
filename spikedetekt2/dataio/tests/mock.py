e2c6519a3ae01ef29b0a74c6d2cf5e098d03fcdd
Cyrille Rossant <cyrille.rossant@gmail.com>
2013-10-18 11:15:30 +0200

    Put mock experiment creation in the mock module.

--- spikedetekt2/dataio/tests/mock.py
+++ spikedetekt2/dataio/tests/mock.py
@@ -5,6 +5,7 @@
 import numpy as np
 
 from spikedetekt2.dataio.kwik_creation import *
+from spikedetekt2.dataio.experiment import create_experiment
 
 
 # -----------------------------------------------------------------------------
@@ -69,4 +70,55 @@ def mock_kwik():
             ),
         ],
     )
+    
+def mock_experiment(dir=None, name=None):
+    channel_groups_info = [
+            {
+                'name': 'first channel group',
+                'graph': [[0, 1]],
+                'channels': [
+                    {'name': 'first channel', 'position': (0., 0.)},
+                    {'name': 'second channel', 'position': (1., 2.)},
+                ],
+                'cluster_groups': [
+                    {'name': 'first cluster group',
+                     'clusters': [
+                        {'color': 3}
+                     ]
+                    }
+                ],
+            }
+        ]
+    
+    event_types_info = [
+        {'name': 'my event type', 'color': 2},
+    ]
+    
+    recordings_info = [
+        {
+            'name': 'my first recording',
+            'start_time': 0.,
+            'start_sample': 0,
+            'sample_rate': 20000.,
+            'band_low': 100.,
+            'band_high': 3000.,
+            'bit_depth': 16,
+        }
+    ]
+    
+    spikedetekt_params = {
+        'nwavesamples': 20,
+        'fetdim': 3,
+    }
+    
+    create_experiment(
+        name=name, 
+        dir=dir,
+        channel_groups_info=channel_groups_info,
+        event_types_info=event_types_info,
+        recordings_info=recordings_info,
+        spikedetekt_params=spikedetekt_params)
+    
+    
+    
     
