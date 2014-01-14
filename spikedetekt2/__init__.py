a9180edd743a226309312ec078248683ad57f1fb
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-01-14 14:25:18 +0100

    Adding logging capabilities.

--- spikedetekt2/__init__.py
+++ spikedetekt2/__init__.py
@@ -1,4 +1,40 @@
+# -----------------------------------------------------------------------------
+# Imports
+# -----------------------------------------------------------------------------
+import os
+import sys
+import logging
+
+import spikedetekt2.utils.logger as log
 from utils import *
 from processing import *
 from core import *
 from dataio import *
+
+
+# -----------------------------------------------------------------------------
+# Module constants
+# -----------------------------------------------------------------------------
+__version__ = '0.3.0dev'
+
+APPNAME = 'spikedetekt'
+
+ABOUT = """Spikedetekt detekt spikes.
+
+This software was developed by Cyrille Rossant, Shabnam Kadir, Dan Goodman, Kenneth Harris in the Cortical Processing Laboratory at UCL (http://www.ucl.ac.uk/cortexlab)."""
+
+
+# -----------------------------------------------------------------------------
+# Loggers
+# -----------------------------------------------------------------------------
+LOGGERS = {}
+log.LOGGERS = LOGGERS
+# Console logger.
+LOGGER = log.ConsoleLogger(name='{0:s}.console'.format(APPNAME))
+log.register(LOGGER)
+
+sys.excepthook = log.handle_exception
+
+# Set the logging level.
+log.set_level(logging.DEBUG)
+
