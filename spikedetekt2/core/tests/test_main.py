8159c3aac92e6ab68c29b55bf3127d41bb583188
Cyrille Rossant <cyrille.rossant@gmail.com>
2014-11-04 18:52:28 +0100

    Fixing PCs.

--- spikedetekt2/core/tests/test_main.py
+++ spikedetekt2/core/tests/test_main.py
@@ -47,17 +47,17 @@ prb = {0:
 
 def setup():
     create_files('myexperiment', dir=DIRPATH, prm=prm, prb=prb)
-    
+
     # Open the files.
     files = open_files('myexperiment', dir=DIRPATH, mode='a')
-    
+
     # Add data.
-    add_recording(files, 
+    add_recording(files,
                   sample_rate=sample_rate,
                   nchannels=nchannels)
     add_cluster_group(files, channel_group_id='0', id='0', name='Noise')
     add_cluster(files, channel_group_id='0',)
-    
+
     # Close the files
     close_files(files)
 
@@ -73,9 +73,9 @@ def test_run_nospikes():
     """Read from NumPy array file."""
     # Run the algorithm.
     with Experiment('myexperiment', dir=DIRPATH, mode='a') as exp:
-        run(np.zeros((nsamples, nchannels)), 
+        run(np.zeros((nsamples, nchannels)),
             experiment=exp, prm=prm, probe=Probe(prb))
-    
+
     # Open the data files.
     with Experiment('myexperiment', dir=DIRPATH) as exp:
         assert len(exp.channel_groups[0].spikes) == 0
@@ -86,37 +86,49 @@ def test_run_1():
     # Run the algorithm.
     with Experiment('myexperiment', dir=DIRPATH, mode='a') as exp:
         run(raw_data, experiment=exp, prm=prm, probe=Probe(prb),)
-    
+
     # Open the data files.
     with Experiment('myexperiment', dir=DIRPATH) as exp:
         nspikes = len(exp.channel_groups[0].spikes)
         assert exp.channel_groups[0].spikes.clusters.main.shape[0] == nspikes
         assert exp.channel_groups[0].spikes.features_masks.shape[0] == nspikes
         assert exp.channel_groups[0].spikes.waveforms_filtered.shape[0] == nspikes
-        
+
         assert isinstance(exp.channel_groups[0]._node.pca_waveforms,
             tb.Array)
 
         # Assert the log file exists.
         logfile = exp.gen_filename('log')
         assert os.path.exists(logfile)
-        
+
         assert exp.recordings[0].raw.shape == (nsamples, nchannels)
         assert exp.recordings[0].high.shape == (nsamples, nchannels)
-        assert exp.recordings[0].low.shape[0] in range(nsamples // 16 - 2, 
+        assert exp.recordings[0].low.shape[0] in range(nsamples // 16 - 2,
                                                        nsamples // 16 + 3)
         assert exp.recordings[0].low.shape[1] == nchannels
-    
+
 @with_setup(setup,)
 def test_run_2():
     """Read from .dat file."""
     path = os.path.join(DIRPATH, 'mydatfile.dat')
     (raw_data * 1e4).astype(np.int16).tofile(path)
-    
+
     # Run the algorithm.
     with Experiment('myexperiment', dir=DIRPATH, mode='a') as exp:
         run(path, experiment=exp, prm=prm, probe=Probe(prb))
-    
+
     # Open the data files.
     with Experiment('myexperiment', dir=DIRPATH) as exp:
         assert len(exp.channel_groups[0].spikes)
+
+@with_setup(setup,)
+def test_run_canonical_pcs():
+    prm_canonical = prm.copy()
+
+    canonical_pcs = np.ones((prm['nfeatures_per_channel'],
+                             prm['waveforms_nsamples'],
+                             prm['nchannels']))
+    prm_canonical['canonical_pcs'] = canonical_pcs
+
+    with Experiment('myexperiment', dir=DIRPATH, mode='a') as exp:
+        run(raw_data, experiment=exp, prm=prm_canonical, probe=Probe(prb),)
--- spikedetekt2/processing/pca.py
+++ spikedetekt2/processing/pca.py
@@ -11,16 +11,32 @@ from kwiklib.utils.six.moves import range
 # -----------------------------------------------------------------------------
 # PCA functions
 # -----------------------------------------------------------------------------
-def compute_pcs(x, npcs=None):
+def compute_pcs(x, npcs=None, masks=None):
     """Compute the PCs of an array x, where each row is an observation.
     x can be a 2D or 3D array. In the latter case, the PCs are computed
     and concatenated iteratively along the last axis."""
     # If x is a 3D array, compute the PCs by iterating over the last axis.
     if x.ndim == 3:
-        return np.dstack([compute_pcs(x[..., i], npcs=npcs) 
+        if masks is None:
+            return np.dstack([compute_pcs(x[..., i], npcs=npcs, masks=None)
+                              for i in range(x.shape[-1])])
+        else:
+            # We pass the masks to compute_pcs for each row of the 3D array.
+            assert isinstance(masks, np.ndarray)
+            assert masks.ndim == 2
+            assert masks.shape[0] == x.shape[0]  # number of spikes
+            assert masks.shape[1] == x.shape[-1]  # number of channels
+            return np.dstack([compute_pcs(x[..., i], npcs=npcs,
+                                          masks=masks[..., i])
                               for i in range(x.shape[-1])])
     # Now, we assume x is a 2D array.
     assert x.ndim == 2
+    # Check the masks.
+    if masks is not None:
+        assert masks.ndim == 1
+        assert masks.shape[0] == x.shape[0]  # number of spikes
+        # Only select those rows in x that are *unmasked* (mask>0).
+        x = np.compress(masks>0, x, axis=0)
     # Take the covariance matrix.
     cov_ss = np.cov(x.astype(np.float64), rowvar=0)
     # Compute the eigenelements.
@@ -31,17 +47,17 @@ def compute_pcs(x, npcs=None):
         return pcs[:npcs,...]
     else:
         return pcs
-    
+
 def project_pcs(x, pcs):
     """Project data points onto principal components.
-    
+
     Arguments:
       * x: a 2D array.
       * pcs: the PCs as returned by `compute_pcs`.
-    
+
     """
     x_proj = np.einsum('ijk,jk->ki', pcs, x)  # Notice the transposition.
     x_proj *= 100.
     return x_proj
 
-    
+
